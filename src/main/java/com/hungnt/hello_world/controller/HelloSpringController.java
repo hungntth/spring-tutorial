dc8c84ddca4b95e2bae81e81efef00d8581df41f
hungnt <hungnguyent@kiemsaphia.com>
2024-08-27 17:11:32 +0700

    hello world

---
+++ src/main/java/com/hungnt/hello_world/controller/HelloSpringController.java
@@ -0,0 +1,13 @@
+package com.hungnt.hello_world.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloSpringController {
+    @GetMapping("/hello")
+    String sayHello () {
+        return "Hello spring";
+    }
+
+}
